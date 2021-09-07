99330be1c6313e51244a846414ccef028c351d5c
zhengjr13608 <362675035@qq.com>
2021-09-07 10:30:58 +0800

    first

---
+++ src/main/java/com/farwolf/weex/core/local/Asset.java
@@ -0,0 +1,63 @@
+package com.farwolf.weex.core.local;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.farwolf.util.FileTool;
+import com.taobao.weex.utils.WXFileUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by zhengjiangrong on 2018/5/1.
+ */
+
+public class Asset implements ILocal {
+
+
+
+    public String getString(Context c,String path)
+    {
+        return WXFileUtils.loadAsset(path,c);
+    }
+
+
+    public Bitmap getBitmap(Context c, String path)
+    {
+        return FileTool.loadAssetImage(path,c);
+    }
+
+
+    public boolean isExist(Context c){
+        String q[]= new String[0];
+        try {
+            q = c.getAssets().list("app");
+        } catch (IOException e) {
+            return false;
+        }
+        return q.length>0;
+    }
+
+    public InputStream getFileInputStream(Context c, String path)
+    {
+        try {
+
+            InputStream inputStream = c.getAssets().open(path);
+            return inputStream;
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+}
